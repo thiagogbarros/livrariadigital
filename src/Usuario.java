a1edadf5549d3d7f9488fddcd22565bae8f4f045
Thiago <thiagobarros@cjr.org.br>
2018-06-03 00:34:38 -0300

    Criei os getters and setters da classe usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,13 +1,13 @@
 
 public class Usuario {
-	int cpf;
-	String nome;
-	int idade;
-	String endereco;
-	int telefone;
-	int cep;
+	private int cpf;
+	private String nome;
+	private int idade;
+	private String endereco;
+	private int telefone;
+	private int cep;
 
-	Usuario criar() {
+	public static Usuario criar() {
 		Usuario x = new Usuario();	
 		return x;
 	}
@@ -17,11 +17,60 @@ public class Usuario {
 	}
 	
 	void consultar(Usuario x) {
-		System.out.printf("CPF: \n",x.cpf);
-		System.out.printf("NOME: \n",x.nome);
-		System.out.printf("IDADE: \n",x.idade);
-		System.out.printf("ENDERECO: \n",x.endereco);
-		System.out.printf("TELEFONE: \n",x.telefone);
-		System.out.printf("CEP: \n",x.cep);
+		System.out.printf("CPF: \n",x.getCpf());
+		System.out.printf("NOME: \n",x.getNome());
+		System.out.printf("IDADE: \n",x.getIdade());
+		System.out.printf("ENDERECO: \n",x.getEndereco());
+		System.out.printf("TELEFONE: \n",x.getTelefone());
+		System.out.printf("CEP: \n",x.getCep());
 	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public int getCep() {
+		return cep;
+	}
+
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+	
 }
