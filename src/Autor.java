6aa0e8dd123f647342bd538280f49b78b2aa1714
Thiago <thiagobarros@cjr.org.br>
2018-06-03 01:16:10 -0300

    Adicionando extends na classe Autor

--- src/Autor.java
+++ src/Autor.java
@@ -1,18 +1,13 @@
 
-public class Autor {
+public class Autor extends Usuario {
 	private int id;
-	private String nome;
+
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+
 	
 }
