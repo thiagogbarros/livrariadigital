d9a9567248882167ec61cc657dfe3d23a3c22853
Thiago <thiagobarros@cjr.org.br>
2018-06-03 12:09:18 -0300

    Arrumando classe banco de dados, colocando arraylist

--- src/livrariadigital/BancoDeDados.java
+++ src/livrariadigital/BancoDeDados.java
@@ -1,11 +1,43 @@
 package livrariadigital;
+import java.util.ArrayList;
 
 public class BancoDeDados {
-	private Usuario usuarioBD[];
-	private Genero generoBD[];
-	private Livro livroBD[];
-	private Editora editoraBD[];
-	private Autor autorBD[];
+	private ArrayList< Usuario > usuarioBD= new ArrayList< Usuario >() ;
+	private ArrayList<Genero> generoBD = new ArrayList<Genero>();
+	private ArrayList<Livro> livroBD = new ArrayList<Livro>();
+	private ArrayList<Editora> editoraBD = new ArrayList<Editora>();
+	private ArrayList<Autor> autorBD = new ArrayList<Autor>();
+	
+	public ArrayList<Usuario> getUsuarioBD() {
+		return usuarioBD;
+	}
+	public void setUsuarioBD(ArrayList<Usuario> usuarioBD) {
+		this.usuarioBD = usuarioBD;
+	}
+	public ArrayList<Genero> getGeneroBD() {
+		return generoBD;
+	}
+	public void setGeneroBD(ArrayList<Genero> generoBD) {
+		this.generoBD = generoBD;
+	}
+	public ArrayList<Livro> getLivroBD() {
+		return livroBD;
+	}
+	public void setLivroBD(ArrayList<Livro> livroBD) {
+		this.livroBD = livroBD;
+	}
+	public ArrayList<Editora> getEditoraBD() {
+		return editoraBD;
+	}
+	public void setEditoraBD(ArrayList<Editora> editoraBD) {
+		this.editoraBD = editoraBD;
+	}
+	public ArrayList<Autor> getAutorBD() {
+		return autorBD;
+	}
+	public void setAutorBD(ArrayList<Autor> autorBD) {
+		this.autorBD = autorBD;
+	}
 	
 	
 }
