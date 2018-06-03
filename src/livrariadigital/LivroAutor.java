6610a34150fd48c8b1cc7f75b30db415fb01b543
Thiago <thiagobarros@cjr.org.br>
2018-06-03 20:36:13 -0300

    Arrumei os setters da classe LivroAutor

---
+++ src/livrariadigital/LivroAutor.java
@@ -0,0 +1,21 @@
+
+package livrariadigital;
+
+public class LivroAutor {
+	private int idAutor;
+	private int isbn;
+	public int getIdAutor() {
+		return idAutor;
+	}
+	public void setIdAutor(int idAutor) {
+		this.idAutor = idAutor;
+	}
+	public int getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+	
+
+}
