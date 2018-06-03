89c3f33bc46ff5142cb398c7beaf78b227c2e6ba
Thiago <thiagobarros@cjr.org.br>
2018-06-03 20:39:45 -0300

    Arrumei o setter da classe funcionario

---
+++ src/livrariadigital/Funcionario.java
@@ -0,0 +1,16 @@
+
+package livrariadigital;
+
+public class Funcionario {
+	private int idfuncionario;
+
+	public int getIdfuncionario() {
+		return idfuncionario;
+	}
+
+	public void setIdfuncionario(int idfuncionario) {
+		this.idfuncionario = idfuncionario;
+	}
+	
+
+}
