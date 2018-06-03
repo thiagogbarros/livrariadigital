e283c0dc0a4eb484a10bb1987f65152eef73a9fd
Thiago <thiagobarros@cjr.org.br>
2018-06-03 13:47:31 -0300

    Melhorando métodos e maneira de criar e preencher os arrays na parte de usuario

--- src/livrariadigital/BancoDeDados.java
+++ src/livrariadigital/BancoDeDados.java
@@ -2,19 +2,33 @@ package livrariadigital;
 import java.util.ArrayList;
 
 public class BancoDeDados {
-	private ArrayList< Usuario > usuarioBD= new ArrayList< Usuario >() ;
 	private ArrayList<Genero> generoBD = new ArrayList<Genero>();
 	private ArrayList<Livro> livroBD = new ArrayList<Livro>();
 	private ArrayList<Editora> editoraBD = new ArrayList<Editora>();
 	private ArrayList<Autor> autorBD = new ArrayList<Autor>();
 	
-	public ArrayList<Usuario> getUsuarioBD() {
+	public ArrayList<Usuario> createUsuarioBD() {
+		ArrayList< Usuario > usuarioBD= new ArrayList< Usuario >() ;
 		return usuarioBD;
 	}
-	public ArrayList<Usuario> addUsuarioBD(ArrayList<Usuario> usuarioBD) {
-		this.usuarioBD = usuarioBD;
+	public ArrayList<Usuario> addUsuarioBD(ArrayList<Usuario> usuarioBD, Usuario usuario) {
+		usuarioBD.add(usuario);
 		return usuarioBD;
 	}
+	public ArrayList<Usuario> removeUsuarioBD(ArrayList<Usuario> usuarioBD, int id) {
+		usuarioBD.remove(id);
+		return usuarioBD;
+	}
+	public int getLastIdUsuarioBD(ArrayList<Usuario> usuarioBD) {
+		int id;
+		id = usuarioBD.size()-1;
+		return id;
+	}
+	public void clearUsuarioBD(ArrayList<Usuario> usuarioBD) {
+		usuarioBD.clear();
+	}
+	
+	
 	public ArrayList<Genero> getGeneroBD() {
 		return generoBD;
 	}
