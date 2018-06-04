973f7a23a5c92ac00b99b132a00581738250e0c3
Thiago <thiagobarros@cjr.org.br>
2018-06-03 22:17:56 -0300

    Coloquei static nas classes do banco

--- src/livrariadigital/BancoDeDados.java
+++ src/livrariadigital/BancoDeDados.java
@@ -3,109 +3,109 @@ import java.util.ArrayList;
 
 public class BancoDeDados {
 	
-	public ArrayList<Livro> createLivroBD() {
+	public static ArrayList<Livro> createLivroBD() {
 		ArrayList< Livro > livroBD= new ArrayList< Livro >() ;
 		return livroBD;
 	}
-	public ArrayList<Livro> addLivroBD(ArrayList<Livro> livroBD, Livro livro) {
+	public static ArrayList<Livro> addLivroBD(ArrayList<Livro> livroBD, Livro livro) {
 		livroBD.add(livro);
 		return livroBD;
 	}
-	public ArrayList<Livro> removeLivroBD(ArrayList<Livro> livroBD, int id) {
+	public static ArrayList<Livro> removeLivroBD(ArrayList<Livro> livroBD, int id) {
 		livroBD.remove(id);
 		return livroBD;
 	}
-	public int getLastIdLivroBD(ArrayList<Livro> livroBD) {
+	public static int getLastIdLivroBD(ArrayList<Livro> livroBD) {
 		int id;
 		id = livroBD.size()-1;
 		return id;
 	}
-	public void clearLivroBD(ArrayList<Livro> livroBD) {
+	public static void clearLivroBD(ArrayList<Livro> livroBD) {
 		livroBD.clear();
 	}
 	
 	
-	public ArrayList<Genero> createGeneroBD() {
+	public static ArrayList<Genero> createGeneroBD() {
 		ArrayList< Genero > generoBD= new ArrayList< Genero >() ;
 		return generoBD;
 	}
-	public ArrayList<Genero> addGeneroBD(ArrayList<Genero> generoBD, Genero genero) {
+	public static ArrayList<Genero> addGeneroBD(ArrayList<Genero> generoBD, Genero genero) {
 		generoBD.add(genero);
 		return generoBD;
 	}
-	public ArrayList<Genero> removeGeneroBD(ArrayList<Genero> generoBD, int id) {
+	public static ArrayList<Genero> removeGeneroBD(ArrayList<Genero> generoBD, int id) {
 		generoBD.remove(id);
 		return generoBD;
 	}
-	public int getLastIdGeneroBD(ArrayList<Genero> generoBD) {
+	public static int getLastIdGeneroBD(ArrayList<Genero> generoBD) {
 		int id;
 		id = generoBD.size()-1;
 		return id;
 	}
-	public void clearGeneroBD(ArrayList<Genero> generoBD) {
+	public static void clearGeneroBD(ArrayList<Genero> generoBD) {
 		generoBD.clear();
 	}
 	
-	public ArrayList<Usuario> createUsuarioBD() {
+	public static ArrayList<Usuario> createUsuarioBD() {
 		ArrayList< Usuario > usuarioBD= new ArrayList< Usuario >() ;
 		return usuarioBD;
 	}
-	public ArrayList<Usuario> addUsuarioBD(ArrayList<Usuario> usuarioBD, Usuario usuario) {
+	public static ArrayList<Usuario> addUsuarioBD(ArrayList<Usuario> usuarioBD, Usuario usuario) {
 		usuarioBD.add(usuario);
 		return usuarioBD;
 	}
-	public ArrayList<Usuario> removeUsuarioBD(ArrayList<Usuario> usuarioBD, int id) {
+	public static ArrayList<Usuario> removeUsuarioBD(ArrayList<Usuario> usuarioBD, int id) {
 		usuarioBD.remove(id);
 		return usuarioBD;
 	}
-	public int getLastIdUsuarioBD(ArrayList<Usuario> usuarioBD) {
+	public static int getLastIdUsuarioBD(ArrayList<Usuario> usuarioBD) {
 		int id;
 		id = usuarioBD.size()-1;
 		return id;
 	}
-	public void clearUsuarioBD(ArrayList<Usuario> usuarioBD) {
+	public static void clearUsuarioBD(ArrayList<Usuario> usuarioBD) {
 		usuarioBD.clear();
 	}
 	
-	public ArrayList<Editora> createEditoraBD() {
+	public static ArrayList<Editora> createEditoraBD() {
 		ArrayList< Editora > editoraBD= new ArrayList< Editora >() ;
 		return editoraBD;
 	}
-	public ArrayList<Editora> addEditoraBD(ArrayList<Editora> editoraBD, Editora Editora) {
+	public static ArrayList<Editora> addEditoraBD(ArrayList<Editora> editoraBD, Editora Editora) {
 		editoraBD.add(Editora);
 		return editoraBD;
 	}
-	public ArrayList<Editora> removeEditoraBD(ArrayList<Editora> editoraBD, int id) {
+	public static ArrayList<Editora> removeEditoraBD(ArrayList<Editora> editoraBD, int id) {
 		editoraBD.remove(id);
 		return editoraBD;
 	}
-	public int getLastIdEditoraBD(ArrayList<Editora> editoraBD) {
+	public static int getLastIdEditoraBD(ArrayList<Editora> editoraBD) {
 		int id;
 		id = editoraBD.size()-1;
 		return id;
 	}
-	public void clearEditoraBD(ArrayList<Editora> editoraBD) {
+	public static void clearEditoraBD(ArrayList<Editora> editoraBD) {
 		editoraBD.clear();
 	}
 	
-	public ArrayList<Autor> createAutorBD() {
+	public static ArrayList<Autor> createAutorBD() {
 		ArrayList< Autor > autorBD= new ArrayList< Autor >() ;
 		return autorBD;
 	}
-	public ArrayList<Autor> addAutorBD(ArrayList<Autor> autorBD, Autor Autor) {
+	public static ArrayList<Autor> addAutorBD(ArrayList<Autor> autorBD, Autor Autor) {
 		autorBD.add(Autor);
 		return autorBD;
 	}
-	public ArrayList<Autor> removeAutorBD(ArrayList<Autor> autorBD, int id) {
+	public static ArrayList<Autor> removeAutorBD(ArrayList<Autor> autorBD, int id) {
 		autorBD.remove(id);
 		return autorBD;
 	}
-	public int getLastIdAutorBD(ArrayList<Autor> autorBD) {
+	public static int getLastIdAutorBD(ArrayList<Autor> autorBD) {
 		int id;
 		id = autorBD.size()-1;
 		return id;
 	}
-	public void clearAutorBD(ArrayList<Autor> autorBD) {
+	public static void clearAutorBD(ArrayList<Autor> autorBD) {
 		autorBD.clear();
 	}
 	
