50f26029fa76ce11bb30bc8282e6f2f7de9c74f4
Victor Alves <victor_xd97@yahoo.com.br>
2018-06-12 18:46:24 -0300

    Add files via upload

---
+++ src/livrariadigital/Carrinho.java
@@ -0,0 +1,22 @@
+package livrariadigital;
+
+public class Carrinho {
+	int IDCliente;
+	int ISBNLivro;
+	
+	public void addLivro() {
+		
+	}
+	
+	public void removeLivro() {
+		
+	}
+	
+	public void esvaziarCarrinho() {
+		
+	}
+	
+	public void finalizarCompra() {
+		
+	}
+}
